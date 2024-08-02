911a0c1b7a6245c5e2dd11c17a63acae02a97b5e
amiraa123amir <alasaad1234alasaad@gmail.com>
2024-08-03 01:30:59 +0300

    amir1 branch

---
+++ database/migrations/2024_08_02_214636_create_ratings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->id();
+          $table->foreignId('doctor_id')
+            ->references('id')
+            ->on('doctors')
+            ->onDelete('cascade');
+            $table->foreignId('user_id')
+            ->references('id')
+            ->on('users')
+            ->onDelete('cascade');
+            $table->integer('rate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ratings');
+    }
+};
